3b160c253576ff7c93c3b809a3b8ef08853130a9
Abhineeth-23 <saiabhineeth23@gmail.com>
2026-03-24 15:54:06 +0530

    changed env variables

--- backend/app/database.py
+++ backend/app/database.py
@@ -5,9 +5,8 @@ from dotenv import load_dotenv
 
 load_dotenv
 
-#SQLALCHEMY_DATABASE_URL = os.environ.get("DATABASE_URL")
+SQLALCHEMY_DATABASE_URL = os.environ.get("DATABASE_URL")
 
-SQLALCHEMY_DATABASE_URL = "postgresql://neondb_owner:npg_AfGUgNLn36ZV@ep-dry-block-a15f13z3-pooler.ap-southeast-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL, 
     pool_pre_ping=True, 
