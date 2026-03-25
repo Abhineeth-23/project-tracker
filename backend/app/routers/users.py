4ae691db7c65dfb8cd117583f4d64c6a813698ee
Abhineeth-23 <saiabhineeth23@gmail.com>
2026-03-25 10:24:10 +0530

    fixed missing import

--- backend/app/routers/users.py
+++ backend/app/routers/users.py
@@ -3,6 +3,7 @@ from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 from ..database import get_db
 from .. import models, schemas
+from typing import List
 
 router = APIRouter(prefix="/api/users", tags=["Users"])
 
